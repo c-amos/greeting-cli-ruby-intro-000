8f2fa85b622a2547e9db665d003e2920a153c353
Corbin A <corbin_amos@yahoo.com>
2020-06-20 12:27:11 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,4 @@
 # code the #greeting method here!
-def greeting(name = "Ruby Programmer")
-  print "Hello, #{name}. It's nice to meet you."
+def greeting(person = "Ruby Programmer")
+  print "Hello, #{person}. It's nice to meet you."
 end
